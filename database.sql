f35fcfdcd4114f7589047b8ddbf6518dd44f177e
Egor Moskaliov <58050140+duckbilled395@users.noreply.github.com>
2022-08-04 14:58:55 +0300

    expres postgresql10

---
+++ database.sql
@@ -0,0 +1,15 @@
+
+
+create TABLE person(
+    id SERIAl PRIMARY KEY,
+    name VARCHAR(255),
+    surname VARCHAR(255)
+);
+
+create TABLE post(
+    id SERIAl PRIMARY KEY,
+    title VARCHAR(255),
+    content VARCHAR(255),
+    user_id INTEGER,
+    FOREIGN KEY (user_id) REFERENCES person (id)
+);
